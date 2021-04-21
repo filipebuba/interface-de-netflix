436beaa73a6aac42b4b23e382f46633d45ee8c85
filipebuba <fbg67744@hotmail.com>
2021-04-21 02:27:36 -0300

    bookflix
    criar uma página de leitura e escuta de um livro

---
+++ enriqu/js/owl/main.js
@@ -0,0 +1,79 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+function pequeno() {
+    window.location.href = "../o-peq-prin/index1.html";
+}
+
+function amigo() {
+    window.location.href = "../amigo/index.html";
+}
+function arte() {
+    window.location.href = "../arte/index.html";
+}
+function como() {
+    window.location.href = "../como/index.html";
+}
+function enriq() {
+    window.location.href = "../enriqu/index.html";
+}
+function hak() {
+    window.location.href = "../haker/index.html";
+}
+
+function maqui() {
+    window.location.href = "../maquiav/index.html";
+}
+
+function mente() {
+    window.location.href = "../mente/index.html";
+}
+
+function pare() {
+    window.location.href = "../pare/index.html";
+}
+function pense() {
+    window.location.href = "../pense/index.html";
+}
+
+function psi() {
+    window.location.href = "../psiq/index.html";
+}
+
+function fbi() {
+    window.location.href = "../fbi/index.html";
+}
+
+function mito() {
+    window.location.href = "../mito/index.html";
+}
+
+function senhor() {
+    window.location.href = "../o-senhor/index.html";
+}
+
+function poder() {
+    window.location.href = "../poder/index.html";
+}
+
+function repr() {
+    window.location.href = "../reprograme/index.html";
+}
+
+function harry() {
+    window.location.href = "../harry-pot/index.html";
+}
